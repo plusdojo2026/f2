c5883ea171bdf522f823ab94d48c7c02a507e5f1
plusdojo2026-watanabe-hi <watanabe-hibiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 08:39:58 +0900

    メニュー画面関連の一部修正

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -39,6 +39,11 @@ public class LoginServlet extends HttpServlet {
 		if(judge == true) {
 			// セッションを取得（なければ作成）
 			HttpSession session = request.getSession();
+			
+			// すでにログイン中のユーザーがいる場合は user_no を削除
+			if (session.getAttribute("user_no") != null) {
+			    session.removeAttribute("user_no");
+			 }
 			  
 			// ログインしたユーザー番号をセッションに保存
 			session.setAttribute("user_no", dao.getUserNo(user));
@@ -46,7 +51,7 @@ public class LoginServlet extends HttpServlet {
 			//menu.jsp にフォワードする
 			response.sendRedirect("/MenuServlet");
 		}else {
-			//login.jsp にフォワードする
+			//何らかの理由でログイン失敗。login.jsp にフォワードする
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 			dispatcher.forward(request, response);
 		}
--- source/src/main/java/servlet/LogoutServlet.java
+++ source/src/main/java/servlet/LogoutServlet.java
@@ -25,7 +25,8 @@ public class LogoutServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
-
+		
+		// セッションを破棄
 		if (session != null) {
 		    session.removeAttribute("user_no");
 		}
