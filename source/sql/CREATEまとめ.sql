d04698c0550ae502eb0872541f4e46dc063bd478
plusdojo2026-watanabe-hi <watanabe-hibiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 13:43:33 +0900

    基本フォルダとSQL文追加

---
+++ source/sql/CREATEまとめ.sql
@@ -0,0 +1,52 @@
+上からCREATEするんやぞ！ 絶対やぞ（戒め）！
+上からCREATEするんやぞ！ 絶対やぞ（戒め）！
+上からCREATEするんやぞ！ 絶対やぞ（戒め）！
+
+◆ログイン関連
+CREATE TABLE users(
+user_no INT AUTO_INCREMENT PRIMARY KEY,
+mail_add VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL
+);
+
+◆年代関連
+CREATE TABLE eras(
+era_no INT AUTO_INCREMENT PRIMARY KEY,
+era_name VARCHAR(4) UNIQUE
+);
+
+◆ジャンル関連
+CREATE TABLE genres(
+genre_no INT AUTO_INCREMENT PRIMARY KEY,
+genre_name VARCHAR(5) UNIQUE
+);
+
+◆単語関連
+CREATE TABLE words(
+word_no INT AUTO_INCREMENT PRIMARY KEY,
+word VARCHAR(100) UNIQUE,
+pronounce VARCHAR(100),
+meaning VARCHAR(100),
+era_no INT,
+genre_no INT,
+level INT,
+FOREIGHN KEY(era_no) REFERENCES era(era_no),
+FOREIGHN KEY(genre_no) REFERENCES genre(genre_no)
+);
+
+◆ユーザーごとにどの単語の閲覧履歴があるか(チェック)
+CREATE TABLE check(
+user_no INT NOT NULL,
+word_no INT NOT NULL,
+PRIMARY KEY(user_no,word_no),
+FOREIGHN KEY(user_no) REFERENCES user(user_no),
+FOREIGHN KEY(word_no) REFERENCES word_list(word_no)
+);
+
+◆ジャンルごとのテンプレート文
+CREATE TABLE template(
+sentence_no INT AUTO_INCREMENT PRIMARY KEY,
+genre_no INT NOT NULL,
+sentence VARCHAR(30) NOT NULL,
+FOREIGN KEY (genre_no) REFERENCES genres(genre_no)
+);
---
+++ source/sql/INSERTまとめ1.sql
@@ -0,0 +1,50 @@
+◆usersテーブル
+INSERT INTO users(mail_add,password) VALUES
+('abcd@gmail.com','password');
+
+
+◆erasテーブル
+INSERT INTO eras(era_name) VALUES
+('昭和後期'),
+('平成初期'),
+('平成中期'),
+('平成後期'),
+('令和');
+
+
+◆genresテーブル
+INSERT INTO genres(genre_name) VALUES
+('用語'),
+('物品・サービス'),
+('人物'),
+('ゲーム・アニメ'),
+('音楽');
+
+
+◆wordsテーブル
+長すぎるので、INSERTまとめ2.sql をみてね☆。
+
+
+◆checkテーブル
+用語を検索した際にその度々追加していくのでここでは追加しない。
+DAOに書く文は、
+
+SELECT * FROM check WHERE user_no = ? AND word_no ?;
+
+で、該当ユーザーがその用語を閲覧したことがあるかを判断。ヒットすれば、あるということ。以下の処理はスキップ。
+ヒットしないなら、見たことがないということだから、以下の
+
+INSERT INTO check(user_no,word_no) VALUES (?,?);
+
+でユーザー ? さんが、この単語を ? 閲覧しました。と表すためにcheckテーブルに要素追加をする。
+両文共に、user_noはServletでセッション取得、word_noはjspから取得する。
+
+
+◆templatesテーブル
+INSERT INTO templates(genre_no,sentence) VALUES
+(1,'って流行った？'),
+(2,'って流行った？'),
+(3,'って誰？'),
+(4,'ってどんな作品？'),
+(5,'ってどんな感じのグループ？');
+
