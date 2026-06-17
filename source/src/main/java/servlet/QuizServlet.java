7456bcd637f55315063930f847a17585f5bc9575
plusdojo2026-iwata <iwata-ryusei-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 11:31:44 +0900

    Quiz GestureServletです

--- source/src/main/java/servlet/QuizServlet.java
+++ source/src/main/java/servlet/QuizServlet.java
@@ -2,11 +2,14 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.WordsDAO;
 
 /**
  * Servlet implementation class QuizServlet
@@ -17,6 +20,19 @@ public class QuizServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// ランダムに一つデータを取得してquiz.jspに返す処理
+		
+		//文字化け防止のエンコーディング
+		request.setCharacterEncoding("UTF-8");
+		
+		WordsDAO wDao = new WordsDAO();
+		String word = wDao.getTheme();
+		
+		//取得情報をリクエストスコープに格納する
+		request.setAttribute("word", word);
+		
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/quiz.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
