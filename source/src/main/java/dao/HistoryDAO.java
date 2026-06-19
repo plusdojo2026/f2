62bc642c343e79178a0e6b65b4cdd7892903face
plusdojo2026-baba <baba-nozomu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 10:30:34 +0900

    history関係

--- source/src/main/java/dao/HistoryDAO.java
+++ source/src/main/java/dao/HistoryDAO.java
@@ -1,5 +1,26 @@
 package dao;
 
-public class HistoryDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import dto.History;
+
+public class HistoryDAO {
+	public boolean check(History hist) {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f2?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+			
+			// SQL文を作成する
+			String sql = "SELECT user_no,word_no FROM history WHERE mail_add = ? AND password = ?";
+		}catch(Exception e) {
+			
+		}
+	}
 }
--- source/src/main/java/dto/History.java
+++ source/src/main/java/dto/History.java
@@ -3,5 +3,21 @@ package dto;
 import java.io.Serializable;
 
 public class History implements Serializable{
-
+	private int user_no;
+	private int word_no;
+	
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public int getWord_no() {
+		return word_no;
+	}
+	public void setWord_no(int word_no) {
+		this.word_no = word_no;
+	}
+	
+	
 }
